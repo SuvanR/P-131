fc0f2371ad5df7bcf3360556d44f1c5ddc035752
SuvanR <73233233+SuvanR@users.noreply.github.com>
2021-06-02 16:29:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function change_to_bedroom() {
+    window.location = "bedroom.html";
+}
+
+function change_to_ac() {
+    window.location = "ac.html";
+}
+
+function change_to_aquarium() {
+    window.location = "aquarium.html";
+}
+
+function change_to_calendar() {
+    window.location = "calendar.html";
+}
+
+function change_to_lamp() {
+    window.location = "lamp.html";
+}
